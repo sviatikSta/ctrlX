c2dc4dd650e1f46253d1a4648fcd45f3aa712171
neveganv <victorianastiuk@gmail.com>
2020-05-02 23:26:55 +0300

    homework methods

---
+++ task5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Завдання_5
+{
+    class Student
+    {
+        public void scholarship(double month1)
+        {
+            Console.WriteLine("Ваша стипендія за 1 місяць склала - ", month1);
+        }
+
+        public void scholarship(double month1, double month2)
+        {
+            Console.WriteLine("Ваша стипендія за 2 місяці склала - {0}", (month1+month2));
+        }
+
+        public void scholarship(double month1, double month2, double month3)
+        {
+            Console.WriteLine("Ваша стипендія за 3 місяці склала - {0}", (month1+month2+month3));
+        }
+
+        public void scholarship(double month1, double month2, double month3, double month4)
+        {
+            Console.WriteLine("Ваша стипендія за 4 місяці склала - {0}", (month1+month2+month3+month4));
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Object1 = new Student();
+            Object1.scholarship(1467.78, 1453.21);
+        }
+    }
+}
---
+++ tasks6-7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Завдання_6_7
+{
+    public static class Matem //статичний клас 
+    {
+        public static void Info(int[] Array, int i) 
+        {
+            Console.WriteLine(Array[i]);
+        }
+
+        public static int Max(int[] Array)
+        {
+            int max = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (max < Array[i]) max = Array[i];
+            }
+            return max;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] A = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                A[i] = rnd.Next(0, 50);
+                Matem.Info(A, i); 
+            }
+
+            Console.WriteLine("Максимальне значення - {0}", Matem.Max(A)); 
+        }
+    }
+}
